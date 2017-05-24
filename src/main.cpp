6b731848e41eda724c228ec4775a3798225356c7
Sm94 <Sm94@github.com>
2017-05-24 08:51:34 +0100

    implemented new serial communication method

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <FastCRC.h>
 
 
 // usually the rotary encoders three pins have the ground pin in the middle
@@ -9,7 +10,26 @@ encoderPinB = 3,   // left (labeled CLK on our decoder, green wire)
 clearButton = 8    // switch (labeled SW on our decoder, orange wire)
 // connect the +5v and gnd appropriately
 };
+enum Modes {
+solidFill = 5000,
+rainbow = 5010
+};
 
+#define PIN 4
+#define LED_COUNT 30
+
+// total size 255
+struct packet{
+  byte start;
+  byte type;
+  uint8_t packetNum;
+  uint16_t CRC;
+  byte data[250];
+}*currentPacket;
+
+volatile byte packetData[256];
+volatile bool read = true;
+uint8_t byteCounter = 0;
 volatile unsigned int encoderPos = 0;  // a counter for the dial
 unsigned int lastReportedPos = 1;   // change management
 static boolean rotating=false;      // debounce management
@@ -18,14 +38,11 @@ static boolean rotating=false;      // debounce management
 boolean A_set = false;
 boolean B_set = false;
 
-#define PIN 4
-#define LED_COUNT 30
-
+FastCRC16 CRC16;
 
 CRGBArray<LED_COUNT> leds;
-
 CRGB current_colour;
-boolean change[3];
+
 boolean off = false;
 
 void doEncoderA();
@@ -33,6 +50,7 @@ void doEncoderB();
 void makeColorGradient(float frequency1, float frequency2, float frequency3,
                            int phase1, int phase2, int phase3);
 boolean numcheck(int R, int G, int B);
+void sendPacket(byte type, byte *data, uint8_t dataSize);
 
 void setup() {
 
@@ -52,10 +70,8 @@ attachInterrupt(0, doEncoderA, CHANGE);
 attachInterrupt(1, doEncoderB, CHANGE);
 
 Serial.begin(9600);  // output
-
-change[0] = true;
-change[1] = false;
-change[2] = false;
+//lets go fast
+//Serial.begin(288000);
 current_colour = CRGB::Black;
 leds.fill_solid(current_colour);
 FastLED.show();
@@ -88,8 +104,38 @@ if (digitalRead(clearButton) == LOW )  {
   FastLED.show();
   delay(200);
 }
+if(!read){
+  byte *data;
+  switch(currentPacket->type){
+    case 0x01:
+      data = (byte*)malloc(3);
+      data[0] = lowByte(LED_COUNT);
+      data[1] = highByte(LED_COUNT);
+      data[2] = '\0';
+      sendPacket(0x01, data, sizeof(data));
+      free(data);
+      break;
+  case 0xFF:
+    sendPacket(0x0F, (byte*)F("ArduinoColourChangeAdvanced_V2.0\0"),35);
+    break;
+  case 0x03:
+    current_colour.r = currentPacket->data[0];
+    current_colour.g = currentPacket->data[1];
+    current_colour.b = currentPacket->data[2];
+    leds.fill_solid(current_colour);
+    FastLED.show();
+    data = (byte*)malloc(4);
+    data[0] = current_colour.r;
+    data[1] = current_colour.g;
+    data[2] = current_colour.b;
+    data[3] = '\0';
+    sendPacket(0x02, data, sizeof(current_colour));
+    free(data);
+    break;
 
-if(Serial.available() > 0)
+}
+}
+/*if(Serial.available() > 0)
   {
       int check = Serial.parseInt();
       int R =  Serial.parseInt();
@@ -114,7 +160,7 @@ if(Serial.available() > 0)
         Serial.print(F("Rainbow"));
         Serial.flush();
       }
-  }
+  }*/
 }
 
 // Interrupt on A changing state
@@ -165,4 +211,37 @@ boolean numcheck(int R, int G, int B){
 if(R < 0 || R > 255 || G < 0 || G > 255 || B < 0 || B > 255 )
   return false;
 return true;
+}
+
+void sendPacket(byte type, byte *data, uint8_t dataSize){
+  packet sendPacket;
+  sendPacket.start = 0xFF;
+  sendPacket.type = type;
+  sendPacket.packetNum = 0;
+  memcpy(sendPacket.data, data, dataSize);
+  sendPacket.CRC = CRC16.ccitt(sendPacket.data, sizeof(sendPacket.data));
+  byte *sendBytes = (byte*)&sendPacket;
+  Serial.write(*sendBytes);
+}
+
+void serialEvent(){
+
+  while(Serial.available()){
+    byte inByte = Serial.read();
+    if(inByte == 0xFF || byteCounter > 0){
+    packetData[byteCounter] = inByte;
+    if(inByte == '\n'){
+      packetData[byteCounter+1] = '\0';
+      currentPacket = (packet*) packetData;
+      if(CRC16.ccitt(currentPacket->data, sizeof(currentPacket->data)) != currentPacket->CRC){
+        Serial.write("error");
+      }
+      byteCounter = 0;
+      read = false;
+    }else{
+      byteCounter++;
+    }
+
+  }
+}
 }
